5a038c3ed20477288a0aab890ed6cb33b1dabc25
welton ramos <welton.silva6@senaisp.edu.br>
2023-07-26 21:55:17 -0300

    Revisao variaveis

---
+++ script.js
@@ -0,0 +1,18 @@
+var nome = "Samuel L. Jackson"
+let idade = 45
+const anoNascimento = 1977
+let idadeEsposa = "45"
+
+console.log(idade == idadeEsposa)
+
+/*
+= atribuição
+== compara se é igual
+=== compara se é exatamente igual
+*/
+
+let listaCompras = ["Maçã", "Manga", "Maracujá", 12, 17, 19.59]
+
+console.log(listaCompras)
+
+listaCompras.forEach(item => console.log(item))
